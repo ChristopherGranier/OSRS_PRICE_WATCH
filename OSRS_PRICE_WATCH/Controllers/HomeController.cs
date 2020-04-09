78fcdcd17fd0b8312f9d37873de98937990655d8
Christopher Granier <cgranier021578@students.iticollege.edu>
2020-04-08 20:14:05 -0500

    Added Controller and displayed a test ItemID

---
+++ OSRS_PRICE_WATCH/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OSRS_PRICE_WATCH.Models;
+
+namespace OSRS_PRICE_WATCH.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Items test = new Items();
+            test.ItemID = "123r3";
+            return View(test);
+        }
+    }
+}
