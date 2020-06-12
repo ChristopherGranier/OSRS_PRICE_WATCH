ae20465e15c75ba2ee533937afc8ebd023965fdf
Christopher Granier <cgranier021578@students.iticollege.edu>
2020-06-11 20:37:35 -0500

    Added support for multiple items to be returned from API. Categories

--- OSRS_PRICE_WATCH/Controllers/HomeController.cs
+++ OSRS_PRICE_WATCH/Controllers/HomeController.cs
@@ -27,12 +27,11 @@ namespace OSRS_PRICE_WATCH.Controllers
         public ActionResult Index()
         {
             var url = "https://api.osrsbox.com/items?where={ \"name\": \"Abyssal whip\", \"duplicate\": false }";
-            _Items test1 = new _Items();
-            Items test = new Items();
-            test1 = Test.Download_serialized_json_data(url);
-            test.Name = test1.name;
-
-            return View(test);
+            List<_Items> test1 = new List<_Items>();
+            test1.Add(Test.Download_serialized_json_data(url));
+            url = "https://api.osrsbox.com/items?where={ \"name\": \"Twisted bow\", \"duplicate\": false }";
+            test1.Add(Test.Download_serialized_json_data(url));
+            return View(test1);
         }
 
         [HttpPost]
@@ -62,5 +61,16 @@ namespace OSRS_PRICE_WATCH.Controllers
 
             return View(test);
         }
+
+        //public IActionResult Search(Items item)
+        //{
+        //    var url = "https://api.osrsbox.com/items?where={ \"name\": \"" + item.Name + "\", \"duplicate\": false }";
+        //    _Items test1 = new _Items();
+        //    List<Items> test = new List<Items>(10);
+        //    test1 = Test.Download_serialized_json_data(url);
+        //    test.Name = test1.name;
+
+        //    return View(test);
+        //}
     }
 }
